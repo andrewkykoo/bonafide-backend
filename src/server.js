53060edd3519b1d9e3a6a713503a74e09e80f4c6
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-06 18:52:10 -0500

    test GET & PUT endpoints

--- src/server.js
+++ src/server.js
@@ -1,11 +1,50 @@
 import express from 'express';
 
+let factsInfo = [
+  {
+    username: 'user1',
+    number: '1',
+    upvotes: 0,
+  },
+  {
+    username: 'user1',
+    number: 2,
+    upvotes: 0,
+  },
+  {
+    username: 'user2',
+    number: 1,
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  res.send(`hi ${req.body.name}`);
+app.put('/api/facts/:usernameAndnumber/upvote', (req, res) => {
+  const { usernameAndnumber } = req.params;
+
+  const fact = factsInfo.find(
+    (fact) => `${fact.username}&${fact.number}` === usernameAndnumber
+  );
+  if (fact) {
+    fact.upvotes += 1;
+    res.send(
+      `${fact.username}'s number ${fact.number} article now has ${fact.upvotes} upvotes`
+    );
+  } else {
+    res.send("the article doesn't exist");
+  }
 });
+
+// app.get('/hello/:name', (req, res) => {
+//   const { name } = req.params;
+//   res.send(`hello ${name}`);
+// });
+
+// app.post('/', (req, res) => {
+//   res.send(`hi ${req.body.name}`);
+// });
 
 app.listen(8000, () => {
   console.log('server listening on port 8000');
